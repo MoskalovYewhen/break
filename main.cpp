ef84221c77d9fe31b37d38a7f61b7218963c72a9
Sala17_uczen <moskalov.yevhen@zst.kolbuszowa.pl>
2023-03-23 11:36:21 +0100

    break

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+// nnnapisz krutkij program wykorysujonce w kodzie break jeśli i=0 i<10,
+//  pentla ma zatczymacz na liczbe 4
+int main()
+{
+    for (int i=0; i < 10; i++)
+{
+        if(i == 5)
+        {
+            break;
+
+        }
+    cout << i ;
+}
+
+    return 0;
+}
